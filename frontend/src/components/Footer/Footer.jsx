c1d8f8b6f846eaf73a81c0465031993606de25c2
PankajKumar2310 <pankajkumarkoli885@gmail.com>
2025-02-18 23:41:37 +0530

    deleted extra files

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Footer.css';
 import { MdOutlineEmail } from "react-icons/md";
 import { CiLocationOn } from "react-icons/ci";
@@ -9,19 +8,21 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-logo-section">
-          <Link to="/" className="footer-logo">
-          <img src={'/images/footericon.png'} alt="logo" />
-          </Link>a
+          <a href="/" className="footer-logo">
+            <img src={'/images/footericon.png'} alt="logo" />
+          </a>
           <div className="footer-contact">
             <div className="footer-contact-email">
-            <MdOutlineEmail />
-            <a href="mailto:contact@adtask.ai" className="contact-link">
-              contact@adtask.ai
-            </a>
+              <MdOutlineEmail />
+              <a href="mailto:contact@adtask.ai" className="contact-link">
+                contact@adtask.ai
+              </a>
             </div>
             <div className="footer-contact-location">
-             <CiLocationOn />
-             <a href="https://www.google.com/maps/place/San+Francisco,+CA/@37.774929,-122.419416,12z/data=!3m1!4b1!4m6!3m5!1s0x8085871576337233:0xf876ccec6b60b48!8m2!3d37.774929!4d-122.419416!16z%3Fhl%3Den" className="contact-link" target="_blank" rel="noopener noreferrer"> San Francisco Bay Area</a>
+              <CiLocationOn />
+              <a href="https://www.google.com/maps/place/San+Francisco,+CA/@37.774929,-122.419416,12z/data=!3m1!4b1!4m6!3m5!1s0x8085871576337233:0xf876ccec6b60b48!8m2!3d37.774929!4d-122.419416!16z%3Fhl%3Den" className="contact-link" target="_blank" rel="noopener noreferrer">
+                San Francisco Bay Area
+              </a>
             </div>
           </div>
         </div>
@@ -30,10 +31,10 @@ const Footer = () => {
           <div className="footer-section">
             <h3>Utilities</h3>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/products">Products</Link></li>
-              <li><Link to="/tools">Tools</Link></li>
-              <li><Link to="/contact">Contact Us</Link></li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/products">Products</a></li>
+              <li><a href="/tools">Tools</a></li>
+              <li><a href="/contact">Contact Us</a></li>
             </ul>
           </div>
 
@@ -55,4 +56,4 @@ const Footer = () => {
   );
 };
 
-export default Footer; 
+export default Footer;
